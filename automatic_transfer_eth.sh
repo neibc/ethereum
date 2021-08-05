33862cacedbca1408e90149aac1926335293f62e
Justin Kim <neibc96@gmail.com>
2021-08-06 00:03:13 +0900

    Update automatic_transfer_eth.sh

--- automatic_transfer_eth.sh
+++ automatic_transfer_eth.sh
@@ -5,14 +5,14 @@
 
 /work/geth/geth --datadir "/work/gethdata" attach << EOF
 
-var fromacc = "0x7E5F4552091A69125d5DfCb7b8C2659029395Bdf"
-var toacc = "0xYOURADDR"
+var fromacc = "0x7E5F4552091A69125d5DfCb7b8C2659029395Bdf";
+var toacc = "0xYOURADDR";
 
 console.log("fromacc: " + fromacc);
 console.log("toacc: " + toacc);
-var gasPrice = new BigNumber(web3.toWei('35', 'gwei'))
+var gasPrice = new BigNumber(web3.toWei('35', 'gwei'));
 console.log("gas price: " + gasPrice);
-var cost = gasPrice.mul(21000)
+var cost = gasPrice.mul(21000);
 console.log("cost price: " + cost);
 var deposit = eth.getBalance(fromacc);
 console.log("deposit: " + deposit);
@@ -20,7 +20,7 @@ var transferval = eth.getBalance(fromacc).sub(cost)
 console.log("transferval: " + transferval);
 
 console.log("Unlock account");
-personal.unlockAccount(fromacc,"YOURPASSWORD")
+personal.unlockAccount(fromacc,"YOURPASSWORD");
 
 if(transferval > 0) {
   console.log("transfer result");
