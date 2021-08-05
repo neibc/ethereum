b75661da7e20251b095815ab2b21e82af2f5d57a
Justin Kim <neibc96@gmail.com>
2021-08-05 23:51:27 +0900

    Create automatic_transfer_erc20token.sh

---
+++ automatic_transfer_erc20token.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+./geth --datadir "/work/gethdata" attach << EOF
+
+var fromacc = "0x7E5F4552091A69125d5DfCb7b8C2659029395Bdf"
+var toacc = "0xYOURADDRESS"
+
+var gasPrice = new BigNumber(web3.toWei('40', 'gwei'))
+console.log("gas price: " + gasPrice);
+var cost = gasPrice.mul(21000)
+console.log("cost price: " + cost);
+
+var minereumAbi = [{"constant":true,"inputs":[],"name":"mintingFinished","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs
+":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_v
+alue","type":"uint256"}],"name":"approve","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"
+uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_value","type":"uint2
+56"}],"name":"transferFrom","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint256"}],"payab
+le":false,"type":"function"},{"constant":false,"inputs":[],"name":"unpause","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":false,"in
+puts":[{"name":"_to","type":"address"},{"name":"_amount","type":"uint256"}],"name":"mint","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"const
+ant":true,"inputs":[],"name":"paused","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address
+"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"finishMinting","outputs":[
+{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"pause","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"f
+unction"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"sym
+bol","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint2
+56"}],"name":"transfer","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_amount","type":"uint256"},{"
+name":"_releaseTime","type":"uint256"}],"name":"mintTimelocked","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"n
+ame":"_owner","type":"address"},{"name":"_spender","type":"address"}],"name":"allowance","outputs":[{"name":"remaining","type":"uint256"}],"payable":false,"type":"functio
+n"},{"constant":false,"inputs":[{"name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"payable":false,"type":"function"},{"anonymous":false,"input
+s":[{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Mint","type":"event"},{"anonymous":false,"inputs":[],"name":"
+MintFinished","type":"event"},{"anonymous":false,"inputs":[],"name":"Pause","type":"event"},{"anonymous":false,"inputs":[],"name":"Unpause","type":"event"},{"anonymous":f
+alse,"inputs":[{"indexed":true,"name":"owner","type":"address"},{"indexed":true,"name":"spender","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"nam
+e":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,
+"name":"value","type":"uint256"}],"name":"Transfer","type":"event"}];
+
+//contract address of ERC-20 token
+var contractaddr = '0x426ca1ea2406c07d75db9585f22781c096e3d0e0';
+var minereumContract = web3.eth.contract(minereumAbi).at(contractaddr);
+
+while(true) {
+var depositval = eth.getBalance(fromacc);
+console.log("depositval: " + depositval);
+
+// need to check the value first before sending them
+if(depositval > 1000) {
+console.log("Unlock account");
+personal.unlockAccount(fromacc,"YOURPASSWORD")
+var valone = web3.fromWei(minereumContract.balanceOf(fromacc));
+console.log("minereum balance: " + valone);
+minereumContract.transfer(toacc,web3.toWei(valone),{from:fromacc});
+}
+
+}
+
+EOF
